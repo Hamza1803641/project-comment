50d29d912bf1e4b30fed2eb8c020c6e9dd654a0b
Hamza Mehmood <mehmood.hamza777@gmail.com>
2025-01-23 19:47:45 +0500

    install python libraries

--- comment classifcation.py
+++ comment classifcation.py
@@ -1,10 +1,10 @@
 
-
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
+
 
 dataset = pd.read_csv('/content/Drugs.csv')
 
@@ -14,7 +14,7 @@ for column in ['Gender', 'BP', 'Cholesterol', 'Drug']:
     dataset[column] = le.fit_transform(dataset[column])
     label_encoders[column] = le
 
-dataset.describe() 
+dataset.describe()
 X = dataset.drop(columns=["Drug"])  # Features
 Y = dataset["Drug"]# Target
 
